0b71c0b407365cc41f1ab6308e92a0e0914d4904
capstonedesign57 <51777153+capstonedesign57@users.noreply.github.com>
2019-06-13 19:18:34 +0900

    Update IntendClassifier.py

--- IntendClassifier.py
+++ IntendClassifier.py
@@ -13,7 +13,7 @@ train_data = train_data[['sentences','intend']]
 train_data = train_data.dropna()
 
 
-# In[4]:
+# In[2]:
 
 
 raw_sents = train_data['sentences']
@@ -24,7 +24,7 @@ for line in raw_sents:
     tokens += word_tokenize(line)
 
 
-# In[5]:
+# In[4]:
 
 
 import numpy as np
@@ -35,7 +35,7 @@ for i in intend :
     vector_intend.append(Untitled4.ans(i))
 
 
-# In[6]:
+# In[5]:
 
 
 import gensim
@@ -53,29 +53,29 @@ model.init_sims(replace=True)
 model.save('atis')
 
 
-# In[7]:
+# In[6]:
 
 
 # https://github.com/mmihaltz/word2vec-GoogleNews-vectors
-# URL¿¡¼­ ¹Ì¸® ÇĞ½ÀµÈ ¸ğµ¨À» ´Ù¿î·Îµå ¹Ş´Â´Ù.
-file_name = 'C:/Users/ÀÌÀ±Àç/Desktop/Á¹¾÷ÇÁ·ÎÁ§Æ®/ex/GoogleNews-vectors-negative300.bin'
+# URLì—ì„œ ë¯¸ë¦¬ í•™ìŠµëœ ëª¨ë¸ì„ ë‹¤ìš´ë¡œë“œ ë°›ëŠ”ë‹¤.
+file_name = 'C:/Users/ì´ìœ¤ì¬/Desktop/ì¡¸ì—…í”„ë¡œì íŠ¸/ex/GoogleNews-vectors-negative300.bin'
 model.intersect_word2vec_format(fname=file_name, binary=True)
 
 
-# In[8]:
+# In[7]:
 
 
 model.save('word2vec.model')
 
 
-# In[9]:
+# In[10]:
 
 
 model = Word2Vec.load('word2vec.model')
 word_vectors = model.wv
 
 
-# In[10]:
+# In[11]:
 
 
 vector_sent = []
@@ -83,36 +83,36 @@ for sent in tokenized_sents:
     vector_sent.append([word_vectors[v] for v in sent])
 
 
-# In[11]:
+# In[12]:
 
 
 import tensorflow as tf
 
 
-# In[12]:
+# In[13]:
 
 
 from keras.preprocessing.sequence import pad_sequences
 from keras import backend as K
-_num_timesteps = 3  #½Ã°è¿­ time step ¼ö
+_num_timesteps = 3  #ì‹œê³„ì—´ time step ìˆ˜
 _dtype = K.floatx()  # backend engine float type
 
 maxlen = 25
 padded = pad_sequences(vector_sent, maxlen, dtype = _dtype, padding='post', value = -1)
 
 
-# In[13]:
+# In[14]:
 
 
 batch_size = 1
 h_size = maxlen
-w_size = 300 #¿öµå º¤ÅÍ Â÷¿ø¼ö (1(h_size) x 300 (w_size))
+w_size = 300 #ì›Œë“œ ë²¡í„° ì°¨ì›ìˆ˜ (1(h_size) x 300 (w_size))
 c_size = 1
 hidden_size = 100 
 out_size = 11
 
 
-# In[14]:
+# In[15]:
 
 
 x_raw = tf.placeholder(tf.float32, shape = [batch_size, h_size, w_size])
@@ -128,7 +128,7 @@ s_init = tf. random_normal(shape=[batch_size, hidden_size], stddev=0.01)
 s[-1] = s_init
 
 
-# In[15]:
+# In[16]:
 
 
 i=0
@@ -142,14 +142,14 @@ o = tf.nn.softmax(tf.matmul(s[i-1], V))
 cost = -tf.reduce_mean(tf.log(tf.reduce_sum(o*ans, axis=1)))
 
 
-# In[16]:
+# In[17]:
 
 
 learning_rate = 0.1
 trainer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
 
-# In[17]:
+# In[18]:
 
 
 sess = tf.InteractiveSession()
@@ -157,18 +157,18 @@ init = tf.global_variables_initializer()
 init.run()
 
 
-# In[18]:
+# In[19]:
 
 
 for _ in range(20):
     for i in range(len(vector_sent)):
-        inputs = padded[i:i+1]  # ¿©±â¼­´Â paddedµÈ -1µéÀ» Àß¶ó³»¾ß
+        inputs = padded[i:i+1]  # ì—¬ê¸°ì„œëŠ” paddedëœ -1ë“¤ì„ ì˜ë¼ë‚´ì•¼
         outputs = vector_intend[i:i+1]
         feed = {x_raw:inputs, ans : outputs}
         trainer.run(feed)
 
 
-# In[19]:
+# In[20]:
 
 
 def accuracy(network, t):
@@ -179,7 +179,7 @@ def accuracy(network, t):
     return tf.reduce_mean(tf.cast(tf.equal(t_predict, t_actual), tf.float32))
 
 
-# In[20]:
+# In[21]:
 
 
 acc = accuracy(o,ans)
